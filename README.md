57862e851cea7d7a86162a8e2ff015fa42f7dcf1
RedLeader12 <redleader@jigsaw.xyz>
2018-05-15 18:45:54 +0800

    Solved the putting letters into array for it to be pritned but questioning the length

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 React test 2 exercise: List and conditionals 
 
-- Create a input field in App with a change listener which outputs the lenght of the entered text below it (in a paragraph)
+- Create a input field in App with a change listener which outputs the length of the entered text below it (in a paragraph)
 - Create a new component (ValidationComponent) which receives text length as a prop
 - Inside the ValidationComponent, either output "text too short" or "text long enought" depending on the text length (5 is the minimum length)
 - Create another component (Char component) and style it as an inline block
--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    letters: [],
+    length: 0
+  }
+
+  letterLengthHandler = (event) => {
+    const letter = event.target.value
+    const letterArray = [ ]
+    letterArray.push(letter)
+    this.setState({letters: letterArray})
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <input type="text" onChange={(event) => this.letterLengthHandler(event)}/>
+        <p> {this.state.length} </p>
       </div>
     );
   }
