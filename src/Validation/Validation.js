1ccbdfe673a54d2044d0b7bb93b79abe19bc58d0
RedLeader12 <redleader@jigsaw.xyz>
2018-05-15 19:06:05 +0800

    Added an if statement to write whether text length was long or short enough

--- src/Validation/Validation.js
+++ src/Validation/Validation.js
@@ -1,9 +1,22 @@
 import React from 'react';
 
 const validation = (props) => {
+
+    let response = null 
+
+    if(props.length < 5){
+        response = (
+            <p>"text too short"</p> 
+        )
+    } else {
+        response = (
+            <p>"text long enough"</p> 
+        )
+    }
     return (
     <div>
         <p> {props.length}</p> 
+        {response}
     </div> 
     )
 };
