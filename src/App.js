48427a84f0931add040bcb8f266b41ac1c3cd29a
RedLeader12 <redleader@jigsaw.xyz>
2018-05-15 19:13:49 +0800

    Characters are done and are pritning out

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Validation from './Validation/Validation'
+import Char from './Char/Char'
 import './App.css';
 
 class App extends Component {
@@ -17,10 +18,18 @@ class App extends Component {
 
 
   render() {
+
+    let letters = this.state.letters.map(letter => {
+      return <Char 
+      letter= {letter}
+      />
+    })
+
     return (
       <div className="App">
         <input type="text" onChange={(event) => this.letterInputHandler(event)}/>
         < Validation length={this.state.length}/>
+        {letters}
       </div>
     );
   }
---
+++ src/Char/Char.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const char = (props) => {
+
+    return (
+    <div>
+        <p> {props.letter}</p> 
+    </div> 
+    )
+};
+
+export default char; 
